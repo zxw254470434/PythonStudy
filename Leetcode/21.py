b6b993c273a0b35120a9d269525990fdc1d26e5b
zxw254470434 <254470434@qq.com>
2023-10-16 21:20:13 +0800

    力扣第21题，双指针解决链表问题。

--- Leetcode/21.py
+++ Leetcode/21.py
@@ -1,2 +1,27 @@
-def mergeTwoLists(list1,list2):
-    return 0
+from ListNode import *
+from typing import *
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        p = dummy
+
+        p1 = list1
+        p2 = list2
+
+        while p1 and p2:
+            if p1.val > p2.val:
+                p.next = p2
+                p2 = p2.next
+            else:
+                p.next = p1
+                p1 = p1.next
+            p = p.next
+
+        if p1:
+            p.next = p1
+        if p2:
+            p.next = p2
+
+        return dummy.next
